b75bb407c75f4d3874324810fe10277f5a9a87d8
chaitsgithub <vuppuluri@gmail.com>
2022-02-13 23:11:54 +0530

    Gorilla Mux Router and Middleware

--- Microservices/coffee-shop/product-api/handlers/products.go
+++ Microservices/coffee-shop/product-api/handlers/products.go
@@ -2,10 +2,12 @@ package handlers
 
 import (
 	"NicJackson/Microservices/coffee-shop/product-api/data"
+	"context"
 	"log"
 	"net/http"
-	"regexp"
 	"strconv"
+
+	"github.com/gorilla/mux"
 )
 
 type Products struct {
@@ -16,24 +18,7 @@ func NewProducts(l *log.Logger) *Products {
 	return &Products{l}
 }
 
-func (p *Products) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	if r.Method == http.MethodGet {
-		p.getProducts(w, r)
-		return
-	}
-	if r.Method == http.MethodPost {
-		p.addProduct(w, r)
-		return
-	}
-	if r.Method == http.MethodPut {
-		p.updProduct(w, r)
-		return
-	}
-
-	http.Error(w, "The api only supports GET and POST. Cannot call with "+r.Method, http.StatusMethodNotAllowed)
-}
-
-func (p *Products) getProducts(w http.ResponseWriter, r *http.Request) {
+func (p *Products) GetProducts(w http.ResponseWriter, r *http.Request) {
 	p.l.Printf("Handle GET Request")
 	lp := data.GetProducts()
 	err := lp.ToJSON(w)
@@ -42,49 +27,51 @@ func (p *Products) getProducts(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func (p *Products) addProduct(w http.ResponseWriter, r *http.Request) {
+func (p *Products) AddProduct(w http.ResponseWriter, r *http.Request) {
 	p.l.Printf("Handle POST Request")
-	prod := &data.Product{}
-	err := prod.FromJSON(r.Body)
-	if err != nil {
-		http.Error(w, "Unable to unmarshall data", http.StatusBadRequest)
-		return
-	}
+	prod := r.Context().Value(KeyProduct{}).(*data.Product)
 	data.AddProduct(prod)
 
-	p.l.Printf("Prod: %#v", prod)
+	w.Write([]byte("Record Added Successfully!"))
 }
 
-func (p *Products) updProduct(w http.ResponseWriter, r *http.Request) {
+func (p *Products) UpdateProduct(w http.ResponseWriter, r *http.Request) {
 	p.l.Printf("Handle PUT Request")
-	// Expect the ID in the path
-	reg := regexp.MustCompile(`/([0-9]+)`)
-	g := reg.FindAllStringSubmatch(r.URL.Path, -1)
-	if len(g) != 1 {
-		http.Error(w, "Invalid URI", http.StatusBadRequest)
-		return
-	}
-	if len(g[0]) != 2 {
-		http.Error(w, "Invalid URI", http.StatusBadRequest)
-	}
-
-	idstring := g[0][1]
-	id, err := strconv.Atoi(idstring)
+	vars := mux.Vars(r)
+	id, err := strconv.Atoi(vars["id"])
 	if err != nil {
 		http.Error(w, "Error converting ID into number", http.StatusBadRequest)
 		return
 	}
 
-	prod := &data.Product{}
-	err = prod.FromJSON(r.Body)
-	if err != nil {
-		http.Error(w, "Unable to unmarshall data", http.StatusBadRequest)
-		return
-	}
+	prod := r.Context().Value(KeyProduct{}).(*data.Product)
 	err = data.UpdateProduct(prod, id)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusOK)
 		return
 	}
 	w.Write([]byte("Record Updated Successfully!"))
+}
+
+type KeyProduct struct{}
+
+func (p *Products) GetProductMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		p.l.Printf("In GetProductMiddleware!")
+		prod := &data.Product{}
+		err := prod.FromJSON(r.Body)
+		if err != nil {
+			http.Error(w, "Unable to unmarshall data", http.StatusBadRequest)
+			return
+		}
+		r = r.WithContext(context.WithValue(r.Context(), KeyProduct{}, prod))
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (p *Products) LoggingMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		p.l.Printf("In LoggingMiddleware!")
+		next.ServeHTTP(w, r)
+	})
 }
--- Microservices/coffee-shop/product-api/main.go
+++ Microservices/coffee-shop/product-api/main.go
@@ -8,14 +8,27 @@ import (
 	"os"
 	"os/signal"
 	"time"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
 	l := log.New(os.Stdout, "product-api-", log.LstdFlags)
 
 	ph := handlers.NewProducts(l)
-	sm := http.NewServeMux()
-	sm.Handle("/", ph)
+	sm := mux.NewRouter()
+
+	getRouter := sm.Methods(http.MethodGet).Subrouter()
+	getRouter.HandleFunc("/", ph.GetProducts)
+	getRouter.Use(ph.LoggingMiddleware)
+
+	putRouter := sm.Methods(http.MethodPut).Subrouter()
+	putRouter.HandleFunc("/{id:[0-9]+}", ph.UpdateProduct)
+	putRouter.Use(ph.LoggingMiddleware, ph.GetProductMiddleware)
+
+	postRouter := sm.Methods(http.MethodPost).Subrouter()
+	postRouter.HandleFunc("/", ph.AddProduct)
+	postRouter.Use(ph.LoggingMiddleware, ph.GetProductMiddleware)
 
 	l.Println("Service Started...")
 
